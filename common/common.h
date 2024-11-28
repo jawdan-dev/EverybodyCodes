61862d505d0a2fd5e8dc4b24871cb7acc1476310
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-11-28 23:19:32 +0000

    Extra stuffs.

--- common/common.h
+++ common/common.h
@@ -3,6 +3,14 @@
 #define nullptr (void*)NULL
 
 #define ARRAY_LENGTH(arr) (sizeof(arr) / sizeof(*arr))
+#define min(a, b) (a <= b ? a : b)
+#define max(a, b) (a >= b ? a : b)
+
+#define ASSERT(condition, msg) \
+	if (condition) { \
+		printf(msg); \
+		exit(1); \
+	}
 
 #include <stdlib.h>
 #include <stdio.h>
